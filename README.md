01ee38a94fb37f4191bfcfc4b2e4f9439ab69882
yangwen <yangwen>
2020-07-27 23:25:49 +0800

    添加学校，邮箱，图像，头像，电话，地址

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 
 #### Random Something
 
-> 随机生成一些内容，包括姓名，文字，身份证，图片，头像，手机号，邮箱等，方便开发者在布局的时候写页面。
+> 随机生成一些内容，包括人名，内容，图片，头像，电话，邮箱，地址，学校，网址等，方便开发者在布局的时候写页面。图片和头像可以直接在线访问。
 
-![Random:内容](images/intro.gif)
+#### 随机人名
+![Random:人名](images/name.gif)
+
+#### 随机内容
+![Random:内容](images/content.gif)
+
+#### 随机地址
+![Random:地址](images/address.gif)
+
+#### 随机电话
+![Random:电话](images/tel.gif)
+
+#### 随机头像
+![Random:头像](images/avatar.gif)
+
+#### 随机图像
+![Random:图像](images/image.gif)
+
+#### 随机学校
+![Random:学校](images/school.gif)
+
+#### 随机邮箱
+![Random:邮箱](images/email.gif)
--- extension.js
+++ extension.js
@@ -4,6 +4,12 @@ const vscode = require('vscode');
 const getRandomName = require('./resources/name');
 const getRandomContent = require('./resources/content');
 const getRandomImage = require('./resources/image');
+const getRandomAvatar = require('./resources/avatar');
+const getRandomEmail = require('./resources/email');
+const getRandomTel = require('./resources/tel');
+const getRandomAddress = require('./resources/address');
+const getRandomSchool = require('./resources/school');
+const getRandomWWW = require('./resources/www');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -18,37 +24,31 @@ function activate(context) {
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let name = vscode.commands.registerCommand('random-something.name', function () {
-    vscode.window.activeTextEditor.edit(editBuilder => {
-      var selections = vscode.window.activeTextEditor.selections
-      selections.forEach(selection => {
-        editBuilder.insert(new vscode.Position(selection.start.line, selection.start.character), getRandomName());
-      })
-    })
-  });
-
-  let content = vscode.commands.registerCommand('random-something.content', function () {
-    vscode.window.activeTextEditor.edit(editBuilder => {
-      var selections = vscode.window.activeTextEditor.selections
-      selections.forEach(selection => {
-        editBuilder.insert(new vscode.Position(selection.start.line, selection.start.character), getRandomContent());
-      })
-    })
-  });
-
-  let image = vscode.commands.registerCommand('random-something.image', function () {
-    vscode.window.activeTextEditor.edit(editBuilder => {
-      var selections = vscode.window.activeTextEditor.selections
-      selections.forEach(selection => {
-        editBuilder.insert(new vscode.Position(selection.start.line, selection.start.character), getRandomImage());
-      })
-    })
-  });
+  // The commandId parameter must match the command field in package.json
   
-	context.subscriptions.push(name);
-	context.subscriptions.push(content);
-	context.subscriptions.push(image);
+  const mapper = {
+    'name': getRandomName,
+    'content': getRandomContent,
+    'image': getRandomImage,
+    'avatar': getRandomAvatar,
+    'email': getRandomEmail,
+    'www': getRandomWWW,
+    'tel': getRandomTel,
+    'school': getRandomSchool,
+    'address': getRandomAddress
+  }
+
+  Object.entries(mapper).forEach(p => {
+    let command = vscode.commands.registerCommand('random-something.'+p[0], function () {
+      vscode.window.activeTextEditor.edit(editBuilder => {
+        var selections = vscode.window.activeTextEditor.selections
+        selections.forEach(selection => {
+          editBuilder.insert(new vscode.Position(selection.start.line, selection.start.character), p[1]());
+        })
+      })
+    });
+    context.subscriptions.push(command);
+  })
 }
 exports.activate = activate;
 
