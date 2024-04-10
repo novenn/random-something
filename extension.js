213b8123824f95af55fc01b0048a2a4bf5ae1bbd
yangwen <yangwen@yangwendeMacBook-Air.local>
2024-04-10 23:13:37 +0800

    feat: 🎸 缓存指令数据，删除冗余代码

--- extension.js
+++ extension.js
@@ -1,72 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const utils = require('./utils')
-const getRandomName = require('./resources/name');
-const getRandomSaying= require('./resources/saying');
-// const getRandomContent= require('./resources/content');
-const getRandomImage = require('./resources/image');
-const getRandomAvatar = require('./resources/avatar');
-const getRandomEmail = require('./resources/email');
-const getRandomTel = require('./resources/tel');
-const getRandomAddress = require('./resources/address');
-const getRandomSchool = require('./resources/school');
-const getRandomWWW = require('./resources/www');
-const getRandomFunny = require('./resources/funny');
 const getRandomNews = require('./resources/news');
-const getRandomVideo = require('./resources/video')
-const getRandomAudio = require('./resources/audio');
-const console = require('console');
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
+const handlers = {}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "random-something" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  
-  // const mapper = {
-  //   'name': getRandomName,
-  //   'saying': getRandomSaying,
-  //   'content': getRandomContent,
-  //   'image': getRandomImage,
-  //   'avatar': getRandomAvatar,
-  //   'email': getRandomEmail,
-  //   'www': getRandomWWW,
-  //   'tel': getRandomTel,
-  //   'school': getRandomSchool,
-  //   'address': getRandomAddress,
-  //   'funny': getRandomFunny,
-  //   'video': getRandomVideo,
-  //   'audio': getRandomAudio,
-  // }
 
   const commands = ['name', 'saying', 'content', 'image', 'avatar','email', 'www', 'tel', 'school', 'address', 'funny', 'video', 'audio']
 
-  // const mapper = {
-  //   'name': "./resources/name",
-  //   'saying': getRandomSaying,
-  //   'content': getRandomContent,
-  //   'image': getRandomImage,
-  //   'avatar': getRandomAvatar,
-  //   'email': getRandomEmail,
-  //   'www': getRandomWWW,
-  //   'tel': getRandomTel,
-  //   'school': getRandomSchool,
-  //   'address': getRandomAddress,
-  //   'funny': getRandomFunny,
-  //   'video': getRandomVideo,
-  //   'audio': getRandomAudio,
-  // }
 
   const inputRules = {
     'video': {
@@ -77,18 +22,23 @@ function activate(context) {
     }
   }
 
-  let configuration = vscode.workspace.getConfiguration('randomSomething')
-  commands.forEach(commandName => {
-    let command = vscode.commands.registerCommand('random-something.'+commandName, function () {
-      var inputRule = inputRules[commandName]
-      let config = configuration.get(commandName)
+  const configuration = vscode.workspace.getConfiguration('randomSomething')
+  commands.forEach( commandName => {
+    const command = vscode.commands.registerCommand('random-something.'+commandName, async () => {
+      const inputRule = inputRules[commandName]
+      const config    = configuration.get(commandName)
 
-      const handler = (...args) => require(`./resources/${commandName}`)(...args)
+      const handler = (...args) => {
+        const instance = handlers[commandName] || require(`./resources/${commandName}`)
+        handlers[commandName] = instance
+        return instance(...args)
+      }
 
       if(inputRule && (!config || !config.length)) {
-        vscode.window.showInputBox(inputRule).then(function(inputMsg) {
-          utils.insert(vscode, handler.bind(null, inputMsg, config))
-        })
+        
+        const input = await vscode.window.showInputBox(inputRule)
+        utils.insert(vscode, handler.bind(null, input, config))
+
       } else {
         utils.insert(vscode, handler.bind(null, config))
       }
@@ -150,9 +100,6 @@ function activate(context) {
   context.subscriptions.push(news);
 }
 
-// exports.activate = activate;
-
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
